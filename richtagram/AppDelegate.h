10e26187bc0d8249625751f8c24cfc2ca2882448
Richbuilder3 <richard.c.grundy@gmail.com>
2014-05-10 14:35:49 -0400

    Initial Commit

---
+++ richtagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  richtagram
+//
+//  Created by Richard Grundy1 on 5/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ richtagramTests/richtagramTests.m
@@ -0,0 +1,34 @@
+//
+//  richtagramTests.m
+//  richtagramTests
+//
+//  Created by Richard Grundy1 on 5/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface richtagramTests : XCTestCase
+
+@end
+
+@implementation richtagramTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
